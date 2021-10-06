d42b4c22c1817367fe9ee56d872be409f342ef6c
hugopi <90212732+hugopi@users.noreply.github.com>
2021-10-06 11:33:15 +0300

    Create task4_rendezVous.adb

---
+++ task4_rendezVous.adb
@@ -0,0 +1,102 @@
+with ada.text_io ; use ada.text_io ;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Numerics.Discrete_Random;
+
+procedure Task4_Rendezvous is
+   
+   --Intint--
+   
+   n : integer := 5;
+   m : integer := 5;
+   subtype hundred is integer range 1..100;
+   Type T_Tableau is array(0..n,0..m) of hundred ;
+   Matr : T_Tableau ;
+
+   package Aleatoire is new Ada.Numerics.Discrete_Random(hundred);
+   use Aleatoire;
+   G : Generator;
+   X : hundred;
+   
+   
+   
+
+   procedure affichageMatrice is
+   begin
+      for i in 0..n loop
+         New_Line;
+         for j in 0..m loop
+            put(Matr(i,j));
+         end loop;
+      end loop;
+   end affichageMatrice;
+   
+   
+   -- task area ---
+   
+   task ListOfRows is
+      entry show;
+   end ListOfRows;
+   task body ListOfRows is
+      Type rowIndexListType is array(0..m) of integer ;
+      rowIndexList : rowIndexListType;
+   begin
+      accept show  do
+         rowIndexList := (0,1,2,3,4,5);
+         for i in 0..3 loop
+            put(rowIndexList(i));
+         end loop;
+      end show;
+   end ListOfRows;
+   
+   
+   
+   
+   
+   
+   task MaxElementInRow is
+      entry findMax;
+   end MaxElementInRow;
+   task body MaxElementInRow is
+      max : Integer := 0;
+   begin
+      accept findMax  do
+         for i in 0..n loop
+            New_Line;
+            max :=0;
+            for j in 0..m loop
+               if Matr(i,j) > max
+               then max := Matr(i,j);
+               end if;
+            end loop;
+            put(max);
+         end loop;
+      end findMax;
+   end MaxElementInRow; 
+  
+   
+
+   
+   -- main begin--
+   
+begin
+   
+   --Matrix implementation--
+   Reset(G);
+   for i in 0..n loop
+      New_Line;
+      for j in 0..m loop
+         X := random(G);
+         Matr(i,j):= X;
+      end loop;
+   end loop ;
+   
+   --test area--
+   
+   
+   
+   --instruction area--
+   affichageMatrice;
+   New_Line;
+   ListOfRows.show;
+   MaxElementInRow.findMax;
+end Task4_Rendezvous;
